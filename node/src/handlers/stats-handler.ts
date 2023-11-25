f83a5783e239d16a2523c7f962c56e91bce91296
T45K <tasktas9@gmail.com>
2023-11-25 12:59:25 +0900

    Fix

--- node/src/handlers/stats-handler.ts
+++ node/src/handlers/stats-handler.ts
@@ -85,12 +85,8 @@ export const getUserStatisticsHandler = [
       let totalLivecomments = 0
       let totalTip = 0
       for (const user of users) {
-        const livestreams = livestreamsByUserId[user.id]
-        if (
-          livestreams === undefined ||
-          livestreams.length === null ||
-          livestreams.length === 0
-        ) {
+        const livestreams = livestreamsByUserId[Number(user.id)] ?? []
+        if (livestreams.length === 0) {
           ranking.push({
             username: user.name,
             score: 0,
